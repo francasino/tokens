954d47dceabef5f0efaca4f560d2e1e752b08876
Fran Casino <35731320+francasino@users.noreply.github.com>
2019-05-22 13:29:28 +0300

    Update MutableTokens.sol

--- MutableTokens.sol
+++ MutableTokens.sol
@@ -23,15 +23,15 @@ contract MutableTokens{
 
     struct Token {
         uint id;
-		bool mutable;
+        bool mutable;
         string name;
         string information;
-		uint timestamp;
+        uint timestamp;
         uint [] pedigreetoken; // id of items that conform this token
         uint [] processestoken; // ids of processes than conform this token
-		uint [] stakeholderstoken; // stakeholders involved
+        uint [] stakeholderstoken; // stakeholders involved
         address owner; // who created the token 
-		bool active; // if necessary, to disable or enable it. 
+        bool active; // if necessary, to disable or enable it. 
         string hashIPFS; // hash of the elements of the struct, for auditing AND IPFS 
     }
     // key is a uint, later corresponding to the token id
@@ -40,196 +40,187 @@ contract MutableTokens{
     mapping(uint => Token) private tokens; // 
 
 
-	struct Pedigree {
+    struct Pedigree {
         uint id; // this especific tuple, containing id and quantity
-        uint id_pedigree_token; // the product
+        uint id_pedigree_token; // the product or token
         uint quantity; // amount of product
         string timestamp; // date of this tuple creation
         address maker; // who creates
-		string hashIPFS; // hash of the elements of the struct, for auditing AND IPFS 
+        string hashIPFS; // hash of the elements of the struct, for auditing AND IPFS 
     }
 
     mapping(uint => Pedigree) private pedigreeChanges; // all the pedigree tuples
-	
-	
-	struct Process {
-        uint id; // this especific process, containing id and quantity
-        uint id_process; // the product
-		uint timestamp; // when it was applied, just in case it is not the same date than token creation
-        address maker; // who applied this proces
-		string hashIPFS; // hash of the elements of the struct, for auditing AND IPFS 
-    }
-
-    mapping(uint => Process) private processChanges; //
-
     
+
+     struct Product {
+        uint id; // this especific tuple, containing id and quantity
+        string name; // the product
+        string description; // the product
+        string timestamp; // date of this tuple creation
+        address maker; // who creates
+        string hashIPFS; // hash of the elements of the struct, for auditing AND IPFS 
+    }
+    mapping(uint => Product) private productChanges; // all the pedigree tuples
+
+
     uint private tokensCount;
+    uint private productCount;
     uint private pedigreeCount;
     uint private processCount;
-	uint private stakeholdersCount;
+    uint private stakeholdersCount;
 
 
     // events, since SC is for global accounts it does not have too much sense but is left here 
     event updateEvent ( // triggers update complete
     );
-	
-	event changeStatusEvent ( // triggers status change
+    
+    event changeStatusEvent ( // triggers status change
     );
 
     address constant public stakeholder = 0xE0f5206BBD039e7b0592d8918820024e2a7437b9; // who registers the token into system. 
-	address constant public stakeholder2 = 0xE0F5206bbd039e7b0592d8918820024E2A743222;
+    address constant public stakeholder2 = 0xE0F5206bbd039e7b0592d8918820024E2A743222;
 
     constructor () public { // constructor, inserts new token in system. we map starting from id=1, hardcoded values of all
-        addToken("Ketchup","Tomato based sauce",1130009112019,stakeholder); //
-		
-        changeStatusCar(1, true);
-        // try addpedigree
+        addToken("Ketchup","Tomato based sauce","1130009112019"); //
+        
     }
-	
-	function addToken (bool memory _mutable, string memory _name, string memory _information, uint _timestamp, address _owner) public {
+    
+    function addToken (bool memory _mutable, string memory _name, string memory _information, string _timestamp) public {
         //require(msg.sender==manufacturer);
 
         tokensCount ++; // inc count at the beginning. represents ID also. 
         tokens[tokensCount].id = tokensCount; 
         tokens[tokensCount].name = _name;
-		tokens[tokensCount].information = _information;
-		tokens[tokensCount].timestamp = _timestamp; // mmhhDDMMYYYY
-        tokens[tokensCount].owner = _owner;
+        tokens[tokensCount].information = _information;
+        tokens[tokensCount].timestamp = _timestamp; // mmhhDDMMYYYY
+        tokens[tokensCount].owner = msg.sender;
         tokens[tokensCount].active = true;
         //tokens[tokensCount].hashIPFS = keccak256(abi.encodePacked(block.number,msg.data, tokens[_tokenId].id, tokens[_tokenId].name, tokens[_tokenId].information, tokens[_tokenId].timestamp, tokens[_tokenId].owner));
     }
-	
-	// add pedigree item to a token, updates hash
-	function addPedigree (uint _tokenId, string memory _id_pedigree_token, uint memory _quantity, string memory _timestamp) public {
+    
+    // add pedigree item to a token, updates hash
+    function addPedigree (uint _tokenId, string memory _id_pedigree_token, uint memory _quantity, string memory _timestamp) public {
         /*require(_tokenId > 0 && _tokenId <= tokensCount);  // security check avoid memory leaks
         require(msg.sender == tokens[_tokenId].owner); //Only token creator can modify it. We can relax this after
-		require(true==tokens[_tokenId].active); //  only if active
+        require(true==tokens[_tokenId].active); //  only if active
         */
-
-		pedigreeCount++;
-		
+        pedigreeCount++;
+        
         pedigreeChanges[pedigreeCount] = Pedigree(pedigreeCount, _id_pedigree_token, _quantity,_timestamp, msg.sender); // the global struct
         tokens[_tokenId].pedigreetoken.push(pedigreeCount); //  we store the reference in the corresponding token
-	
+    
         // update hash
-		//tokens[_tokenId].hashIPFS = keccak256(abi.encodePacked(block.number,msg.data, tokens[_tokenId].id, tokens[_tokenId].name, tokens[_tokenId].information, tokens[_tokenId].timestamp, tokens[_tokenId].owner));
+        //tokens[_tokenId].hashIPFS = keccak256(abi.encodePacked(block.number,msg.data, tokens[_tokenId].id, tokens[_tokenId].name, tokens[_tokenId].information, tokens[_tokenId].timestamp, tokens[_tokenId].owner));
 
         emit updateEvent(); // trigger event 
     }
-	
-	// add process item to a token, updates hash
-	function addProcess (uint _tokenId, string memory _id_pedigree_token, uint memory _quantity, string memory _timestamp) public {
+
+    function addPedigreeElemental (uint _tokenId, string memory _id_product, uint memory _quantity, string memory _timestamp) public {
         /*require(_tokenId > 0 && _tokenId <= tokensCount);  // security check avoid memory leaks
         require(msg.sender == tokens[_tokenId].owner); //Only token creator can modify it. We can relax this after
-		require(true==tokens[_tokenId].active); //  only if active
+        require(true==tokens[_tokenId].active); 
+        require(_id_product > 0 && _id_product <= productCount); //   if product exists
         */
-
-		processCount++;
-		
-        processChanges[processCount] = Process(processCount, _id_process,_timestamp, msg.sender); // the global struct
-        tokens[_tokenId].processtoken.push(processCount); // we store the reference in the corresponding token
-	
+        pedigreeCount++;
+        
+        pedigreeChanges[pedigreeCount] = Pedigree(pedigreeCount, _id_product, _quantity,_timestamp, msg.sender); // the global struct
+        tokens[_tokenId].pedigreetoken.push(pedigreeCount); //  we store the reference in the corresponding token
+    
         // update hash
-		//tokens[_tokenId].hashIPFS = keccak256(abi.encodePacked(block.number,msg.data, tokens[_tokenId].id, tokens[_tokenId].name, tokens[_tokenId].information, tokens[_tokenId].timestamp, tokens[_tokenId].owner));
+        //tokens[_tokenId].hashIPFS = keccak256(abi.encodePacked(block.number,msg.data, tokens[_tokenId].id, tokens[_tokenId].name, tokens[_tokenId].information, tokens[_tokenId].timestamp, tokens[_tokenId].owner));
 
         emit updateEvent(); // trigger event 
     }
-	
-	// add process item to a token, updates hash
-	function addStakeholder (uint _tokenId, string memory _id_pedigree_token, uint memory _quantity, string memory _timestamp) public {
+
+    function addProduct (string memory _name, string memory _description, string memory _timestamp) public {
         /*require(_tokenId > 0 && _tokenId <= tokensCount);  // security check avoid memory leaks
         require(msg.sender == tokens[_tokenId].owner); //Only token creator can modify it. We can relax this after
-		require(true==tokens[_tokenId].active); //  only if active
+        require(true==tokens[_tokenId].active); //  only if active
         */
+        productCount++;
+        
+        productChanges[productCount].id = productCount;
+        productChanges[productCount].name = _name;
+        productChanges[productCount].description = _description;
+        productChanges[productCount].timestamp= _timestamp;
+        productChanges[productCount].maker = msg.sender;
 
-		stakeholdersCount++;
-
-        tokens[_tokenId].stakeholderstoken.push(stakeholdersCount); // we store the reference in the corresponding token
-	
-        // update hash
-		//tokens[_tokenId].hashIPFS = keccak256(abi.encodePacked(block.number,msg.data, tokens[_tokenId].id, tokens[_tokenId].name, tokens[_tokenId].information, tokens[_tokenId].timestamp, tokens[_tokenId].owner));
 
         emit updateEvent(); // trigger event 
     }
-	
-	function changeStatusToken (uint _tokenId, bool _active) public { 
-        /*require(_tokenId > 0 && _tokenId <= tokensCount);  // security check avoid memory leaks
-        require(msg.sender == tokens[_tokenId].owner); //Only token creator can modify it. We can relax this after
-        */
+    
+    function changeStatusToken (uint _tokenId, bool _active) public { 
+        require(_tokenId > 0 && _tokenId <= tokensCount);
 
-		tokens[_tokenId].active = _active;	
+        tokens[_tokenId].active = _active;  
         emit changeStatusEvent(); // trigger event 
     }
-	
-	function retrieveHash (uint _tokenId) public view returns (bytes32){ 
-        //computehash according to unique characteristics
-        // this example hashes a transaction as a whole and info of the car
-		// we can also just hash the info of the car. 
+    
+    function retrieveHash (uint _tokenId) public view returns (bytes32){ 
+        //compute hash according to unique characteristics
+        // this example hashes a transaction as a whole and info
+        // we can also just hash the info of the token. 
         return keccak256(abi.encodePacked(block.number,msg.data, tokens[_tokenId].id, tokens[_tokenId].name, tokens[_tokenId].information, tokens[_tokenId].timestamp, tokens[_tokenId].owner));
     }
-	
-	// getters structs of a token
-	
-	// get the array of pedigrees of a product, later we can loop them using getters to obtain the data
+    
+    // getters structs of a token
+    // get the array of pedigrees of a product, later we can loop them using getters to obtain the data
     function getPedigreeToken (uint _tokenId) public view returns (uint [] memory)  {
         require(_tokenId > 0 && _tokenId <= tokensCount);  // security check avoid memory leaks
         require(msg.sender == tokens[_tokenId].owner); 
        
-
         return tokens[_tokenId].pedigreetoken;
     }
-	
-	// get the array of changes of a product, later we can loop them using getters to obtain the data
+    
+    // get the array of changes of a product, later we can loop them using getters to obtain the data
     function getProcessesToken (uint _tokenId) public view returns (uint [] memory)  {
         require(_tokenId > 0 && _tokenId <= tokensCount);  // security check avoid memory leaks
         require(msg.sender == tokens[_tokenId].owner); 
         
         return tokens[_tokenId].processtoken;
     }
-	
-	// get the array of changes of a product, later we can loop them using getters to obtain the data
+    
+    // get the array of changes of a product, later we can loop them using getters to obtain the data
     function getStakeholderToken (uint _tokenId) public view returns (uint [] memory)  {
         require(_tokenId > 0 && _tokenId <= tokensCount);  // security check avoid memory leaks
         require(msg.sender == tokens[_tokenId].owner); 
         
         return tokens[_tokenId].stakeholderstoken;
     }
-	
-	// getters specific number of story or status
-	function getPedigree (uint _pedigreeId) public view returns (Pedigree memory)  {
+    
+    // getters specific number of story or status
+    function getPedigree (uint _pedigreeId) public view returns (Pedigree memory)  {
         require(_pedigreeId > 0 && _pedigreeId <= pedigreeCount); 
         require(msg.sender==pedigreeChanges[_pedigreeId].maker); // only if he is the author of the content
         
         return pedigreeChanges[_pedigreeId];
     }
 
-	function getProcess (uint _processId) public view returns (Process memory)  {
-        require(_processId > 0 && _processId <= processCount); 
-        require(msg.sender==processChanges[_processId].maker); // only if he is the author of the content
-        
-        return processChanges[_processId];
-    }
-	
+    
     // getters global such as number of structs, for statistics
-	// returns global number of cars, needed to iterate the mapping and to know info.
+    // returns global number of cars, needed to iterate the mapping and to know info.
     function getNumberOfTokens () public view returns (uint){
         
         return tokensCount;
     }
-	
-	// returns global number of stories, needed to iterate the mapping and to know info.
-    function getNumberOfProcesses () public view returns (uint){
+    
+    // returns global number of stories, needed to iterate the mapping and to know info.
+    // smart to smart comm
+    function updateNumberOfProcesses (address addr) public view returns (uint){
         
-        
-        return processCount;
+        Processes p = Processes(addr);
+        return p.getNumberOfProcesses();
+       
     }
-	
-	// returns global number of status, needed to iterate the mapping and to know info.
-    function getNumberOfStakeholders () public view returns (uint){
+    
+    // returns global number of status, needed to iterate the mapping and to know info.
+    // smart to smart comm
+    function updateNumberOfStakeholders (address addr) public view returns (uint){
         
+        Stakeholders s = Stakeholders(addr);
+        return s.getNumberOfStakeholders();
         
-        return stakeholdersCount;
     }
 
-	
+    
 }
